8c6b7b037d90eca671eae4a67911f7485ab123ad
shopifyapp470 <vamjamohit.vhits@gmail.com>
2026-01-06 19:06:11 +0530

    move file route to lib

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -6,7 +6,7 @@ import {
 } from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
-import { syncRewardTemplate } from "./routes/theme-sync.server";
+import { syncRewardTemplate } from "./lib/theme-sync.server";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
