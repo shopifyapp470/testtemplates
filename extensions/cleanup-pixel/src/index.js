81e46144a1c39c2422b3bb81944f38cfb5ee98f0
shopifyapp470 <vamjamohit.vhits@gmail.com>
2026-01-29 17:44:06 +0530

    check browser issue

--- extensions/cleanup-pixel/src/index.js
+++ extensions/cleanup-pixel/src/index.js
@@ -2,9 +2,16 @@ import { register } from "@shopify/web-pixels-extension";
  
 register(({ analytics, browser }) => {
   // 1. UTM capture karke session storage mein save karein
-  analytics.subscribe('page_viewed', async (event) => {
-   console.log('test');
-;  });
+ analytics.subscribe('page_viewed', async (event) => {
+    const url = new URL(event.context.document.location.href);
+    const employeeEmail = url.searchParams.get("utm_source");
+    
+    if (employeeEmail) {
+      // SessionStorage use karein kyunki yeh private mode mein bhi stable hai
+      await browser.sessionStorage.setItem("referred_by_employee", employeeEmail);
+      console.log("🎯 [PIXEL] Referral captured from URL:", employeeEmail);
+    }
+  });
  
   // 2. Checkout complete hone par session clear karein
   analytics.subscribe('checkout_completed', async (event) => {
