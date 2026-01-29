74d07a45941473e6b2b81fc205f306bff09418cb
shopifyapp470 <vamjamohit.vhits@gmail.com>
2026-01-29 17:12:20 +0530

    update check

--- extensions/cleanup-pixel/src/index.js
+++ extensions/cleanup-pixel/src/index.js
@@ -3,16 +3,13 @@ import { register } from "@shopify/web-pixels-extension";
 register(({ analytics, browser }) => {
   // 1. UTM capture karke session storage mein save karein
   analytics.subscribe('page_viewed', async (event) => {
-    const url = new URL(event.context.document.location.href);
-    const employeeEmail = url.searchParams.get("utm_source");
-    if (employeeEmail) {
-      await browser.sessionStorage.setItem("referred_employee", employeeEmail);
-    }
-  });
+   console.log('test');
+;  });
  
   // 2. Checkout complete hone par session clear karein
   analytics.subscribe('checkout_completed', async (event) => {
-    console.log("🏁 [PIXEL] Order complete. Cleaning session storage...");
-    await browser.sessionStorage.removeItem("referred_employee");
+    console.log("🏁 [PIXEL] Order complete. Cleaning storage...");
+    await browser.localStorage.removeItem('referred_by_employee');
+    console.log("✅ [PIXEL] Storage cleared.");
   });
 });
